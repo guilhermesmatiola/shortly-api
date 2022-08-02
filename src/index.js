6e9220bd7f64b14880c4cd22d6d5df7708dd7cfc
guilhermesmatiola <guimatiola@gmail.com>
2022-08-02 17:24:13 -0300

    fix for use

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/indexRouter.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(router);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log('Servidor Online'));
