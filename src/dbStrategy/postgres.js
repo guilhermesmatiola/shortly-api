6155db502e8111870a36ce6c57a9590259410d7d
guilhermesmatiola <guimatiola@gmail.com>
2022-08-05 14:16:00 -0300

    prepare to deploy

--- src/dbStrategy/postgres.js
+++ src/dbStrategy/postgres.js
@@ -4,15 +4,15 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const databaseConfig = {
-   // connectionString: process.env.DATABASE_URL,
-   // ssl:{
-//          rejectUnautorized: false
-   //}
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'euamopalmeiras',
-    database: 'shortly'
+   connectionString: process.env.DATABASE_URL,
+   ssl:{
+         rejectUnautorized: false
+   }
+   //  host: 'localhost',
+   //  port: 5432,
+   //  user: 'postgres',
+   //  password: 'euamopalmeiras',
+   //  database: 'shortly'
 }
 
 const connection = new Pool(databaseConfig);
